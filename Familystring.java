7283fd883fb1f25add09fc577a2e59a1f6d6edab
Heerashrees <nswamy.250@gmail.com>
2024-03-03 16:51:00 +0530

    Family

---
+++ Familystring.java
@@ -0,0 +1,26 @@
+package testpractise;
+
+public class Familystring {
+
+	public static void main(String[] args) {
+		char s[]=new char[4];
+		s[0]='I';
+		s[1]='l';
+		s[2]='M';
+		s[3]='F';
+		
+		
+	
+		for(int i=0;i<=3;i++)
+		{
+			System.out.println(s[i]);
+		}
+
+	
+		
+		
+		
+
+	}
+
+}
