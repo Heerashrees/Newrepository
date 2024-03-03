452b2ecd29422cac50ca41782cf43c9dcc90c703
Heerashrees <nswamy.250@gmail.com>
2024-03-03 15:39:09 +0530

    First

---
+++ Math_area.java
@@ -0,0 +1,17 @@
+package while_prog;
+
+public class Math_area {
+
+	public static void main(String[] args) {
+		double pi=Math.PI;
+		double r=Math.random();
+		for(int i=0;i<11;i++)
+		{
+			double area=pi*r*r;
+			System.out.println(area);
+		}
+		
+	
+	}
+
+}
